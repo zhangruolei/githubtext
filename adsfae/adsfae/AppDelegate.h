2ec257f674b7ef0082bac55ff1ad68ddbf34bdab
zhangruolei <zhangruolei@mofang.com>
2015-11-06 17:49:19 +0800

    1

---
+++ adsfae/adsfae/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  adsfae
+//
+//  Created by zhangruolei on 15/11/6.
+//  Copyright © 2015年 zhangruolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
